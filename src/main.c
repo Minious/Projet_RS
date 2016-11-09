10044e0b0707519ca336a3b687d8f21f7c65b94f
GAMET Ansel <ansel.gamet@telecomnancy.net>
2016-11-09 18:40:23 +0100

    affichage de tout les elements de la struct

--- src/main.c
+++ src/main.c
@@ -10,7 +10,23 @@ int main(int argc, char **argv) {
 	int fd = open("test.tar", O_RDONLY);
 	read(fd, &ma_struct, 512);
 
-	printf("%s\n", ma_struct.name);
+	printf("name %s\n", ma_struct.name);
+	printf("mode %s\n", ma_struct.mode);
+	printf("uid %s\n", ma_struct.uid);
+	printf("gid %s\n", ma_struct.gid);
+	printf("size %s\n", ma_struct.size);
+	printf("mtime %s\n", ma_struct.mtime);
+	printf("checksum %s\n", ma_struct.checksum);
+	printf("typeflag %s\n", ma_struct.typeflag);
+	printf("linkname %s\n", ma_struct.linkname);
+	printf("magic %s\n", ma_struct.magic);
+	printf("version %s\n", ma_struct.version);
+	printf("uname %s\n", ma_struct.uname);
+	printf("gname %s\n", ma_struct.gname);
+	printf("devmajor %s\n", ma_struct.devmajor);
+	printf("devminor %s\n", ma_struct.devminor);
+	printf("prefix %s\n", ma_struct.prefix);
+	printf("pad %s\n", ma_struct.pad);
 
 	return 0;
 }
