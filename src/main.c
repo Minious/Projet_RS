73549b1758f231212161a2701cc8bbc6976a7465
Eliot Godard <eliot.godard@gmail.com>
2016-11-09 18:15:12 +0100

    ça compile !

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "main.h"
 #include "header_posix_ustar.h"
 
 int main(int argc, char **argv) {
 	struct header_posix_ustar ma_struct;
+	int fd;
 	read(fd, &ma_struct, 512);
 
 	return 0;
